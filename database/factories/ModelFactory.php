beae412447d080ef5834bfdd9f27375818073f5e
tp.jigar@gmail.com <Jigar123>
2019-08-25 03:14:07 +0530

    Firebase token (jwt) generated

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -10,10 +10,15 @@
 | database. Just tell the factory how a default model should look.
 |
 */
+use Illuminate\Support\Str;
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
+    	'uniquecode'     => Str::random(32), //$faker->uniquecode, //$faker->unique(),
+        'firstname'     => $faker->name,
+        'lastname'     => $faker->name,
+        'email'    => $faker->unique()->email,
+        'password' => app('hash')->make('12345'), 
+        'app_token' => Str::random(255),
     ];
 });
